20a05b6c0aa8e64d5e11bac26311ed266bae8b78
natalia.moraes <nataliacmorais@hotmail.com>
2021-04-27 00:06:09 -0300

    commit initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ReactiveSearchModule } from './reactive-search/reactive-search.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'busca-reativa',
+    loadChildren:() => import('./reactive-search/reactive-search.module').then(m => m.ReactiveSearchModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
